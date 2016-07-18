e9fe64bd519e70566b541b31f8f91cd31874ae9b
mingz <305603665@qq.com>
2016-07-18 17:21:53 +0800

    init

---
+++ qianzhan_spider/captcha/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding:utf-8 -*-
+__author__ = 'zhaojm'
+
+from captcha import read_gif_img_to_string
--- qianzhan_spider/captcha/captcha.py
+++ qianzhan_spider/captcha/captcha.py
@@ -7,7 +7,7 @@ import numpy
 import pytesseract
 
 
-def convert_to_jpg(gif_file):
+def _convert_to_jpg(gif_file):
     o_filename = gif_file[:-4] + '.jpg'
 
     im = Image.open(gif_file)
@@ -17,7 +17,7 @@ def convert_to_jpg(gif_file):
     return o_filename
 
 
-def format_img(jpg_file):
+def _format_img(jpg_file):
     im = cv2.imread(jpg_file)
 
     vector = [0, 0, 1, 2, 3, 4, 5, 5, 5, 5, 4, 4, 3, 1, 0, 0, -1, -2, -3, -4, -5, -5, -5, -5, -4, -4, -3, -1]  # 右移向量
@@ -32,13 +32,4 @@ def format_img(jpg_file):
 
 
 def read_gif_img_to_string(gif_file):
-    return pytesseract.image_to_string(Image.open(format_img(convert_to_jpg(gif_file))), 'eng+chi_sim')
-
-
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
-    pass
+    return pytesseract.image_to_string(Image.open(_format_img(_convert_to_jpg(gif_file))), 'eng+chi_sim')
