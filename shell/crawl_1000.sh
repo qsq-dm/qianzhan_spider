048772f246f4e7cc6fd750a0cfaac24a9765742b
mingz <305603665@qq.com>
2016-07-21 11:30:26 +0800

    init

--- shell/crawl_1000.sh
+++ shell/crawl_1000.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /home/apps/qianzhan_spider
 dt=$(date "+%Y-%m-%d")
-path_to_log="log/""$dt""_crawl_all.log"
+path_to_log="log/""$dt""_crawl_1000.log"
 echo $path_to_log
 scrapy crawl qianzhan_spider_1000 -s LOG_FILE=$path_to_log &
