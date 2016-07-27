38939a1533d925b112f06df1a7de6f3c5f484ba4
mingz <305603665@qq.com>
2016-07-27 17:47:09 +0800

    init

--- qianzhan_spider_2/setup.py
+++ qianzhan_spider_2/setup.py
@@ -5,6 +5,12 @@ from spider import Spider
 from config import userId, password
 from log import init_logging
 
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+print "sys default encoding: ", sys.getdefaultencoding()
+
 
 def main():
     init_logging()
