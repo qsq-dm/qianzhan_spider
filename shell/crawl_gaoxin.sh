b71684c623f04290335f5d89c1a0507f49870a2f
mingz <305603665@qq.com>
2016-07-29 12:59:23 +0800

    init

--- shell/crawl_gaoxin.sh
+++ shell/crawl_gaoxin.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /home/apps/qianzhan_spider
 dt=$(date "+%Y-%m-%d")
-path_to_log="log/""$dt""_crawl_gaoxin_2.log"
+path_to_log="log/""$dt""_crawl_gaoxin.log"
 echo $path_to_log
-scrapy crawl qianzhan_spider_gaoxin_2 -s LOG_FILE=$path_to_log &
+scrapy crawl qianzhan_spider_gaoxin -s LOG_FILE=$path_to_log &
