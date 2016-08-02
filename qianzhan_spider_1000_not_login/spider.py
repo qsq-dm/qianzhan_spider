77b70de8fd40ffa2c73be62f311822ad302abca5
mingz <305603665@qq.com>
2016-08-02 10:12:21 +0800

    init

--- qianzhan_spider_1000_not_login/spider.py
+++ qianzhan_spider_1000_not_login/spider.py
@@ -143,7 +143,7 @@ class Spider(object):
 
     def _run(self):
 
-        for i in range(62, len(self._txt)):
+        for i in range(63, len(self._txt)):
             for j in range(len(self._txt)):
                 # if i % 2 == 0:
                 #     j = i + 1
