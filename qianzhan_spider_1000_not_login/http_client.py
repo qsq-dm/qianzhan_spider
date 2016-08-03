c590dcee01706ab4c4cc19a56e9bc59cee40598b
mingz <305603665@qq.com>
2016-08-03 10:29:43 +0800

    init

--- qianzhan_spider_1000_not_login/http_client.py
+++ qianzhan_spider_1000_not_login/http_client.py
@@ -4,7 +4,7 @@ __author__ = 'zhaojm'
 import requests
 import time
 
-from config import download_delay
+from config import download_delay, default_headers
 
 import logging
 import json
@@ -15,19 +15,7 @@ from mongo import ProxyDB
 class HTTPClient(object):
     def __init__(self, min_time_interval=download_delay):
         self._session = requests.Session()
-        self._session.headers.update({
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Accept-Encoding': 'gzip, deflate, sdch',
-            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.6',
-            # Cache-Control:no-cache
-            'Connection': 'keep-alive',
-            # 'Cookie': 'qznewsite.uid=rssbv51ml0mwomw4k4mkycz2; qz.newsite=41A3A9C8ADE5F5073B86647E6C185D2272112F0E96AD14B275E442ED8A2B85688F50512A00FAD14DB2D485EF57054E35ECB028B44D155EE32D029A4352C6617B74BE83EB551C4D66024D7F4913B053785964ED37F3061D43BA0E8663791A1E143AC458E738C7484453887CEAB6EDEC359632DDDEAF6D585D0BD31087077296B6D97CF68F',
-            'Host': 'qiye.qianzhan.com',
-            # Pragma:no-cache
-            'Referer': 'http://qiye.qianzhan.com/',
-            'Upgrade-Insecure-Requests': 1,
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
-        })
+        self._session.headers.update(default_headers)
         self._min_time_interval = min_time_interval * 1000
         self._last_request_time = -1
 
