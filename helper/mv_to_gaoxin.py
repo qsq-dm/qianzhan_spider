e0d5504c830482fbbb695063bac0fdd1a0c0725b
mingz <305603665@qq.com>
2016-08-03 16:15:53 +0800

    init

---
+++ helper/mv_to_gaoxin.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+__author__ = 'zhaojm'
+
+import codecs
+
+import pymongo
+
+# MONGO
+MONGO_URI = "localhost:27017"
+
+mongo_client = pymongo.MongoClient(MONGO_URI)
+db = mongo_client["qianzhan"]
+db_1000 = mongo_client["qianzhan_1000"]
+
+if __name__ == "__main__":
+    cur = db_1000.company_info_items_gaoxin.find().batch_size(50)
+    for item in cur:
+        db.company_info_items_gaoxin.update({'company_name': item['company_name']}, {'$set': item}, True, True)
