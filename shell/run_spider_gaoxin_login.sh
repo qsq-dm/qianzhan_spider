6eaa5b629f3dcc5934d6769a1cbeccbfefb7ee06
mingz <305603665@qq.com>
2016-08-03 10:13:39 +0800

    init

--- shell/run_spider_gaoxin_login.sh
+++ shell/run_spider_gaoxin_login.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /home/apps/qianzhan_spider
 dt=$(date "+%Y-%m-%d")
-path_to_log="log/""$dt""_run_spider_2_gaoxin.log"
+path_to_log="log/""$dt""_run_spider_gaoxin_login.log"
 echo $path_to_log
-python qianzhan_spider_2/setup_gaoxin.py &
+python qianzhan_spider_gaoxin_login/setup.py &
