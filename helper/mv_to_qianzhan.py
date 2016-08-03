2dfde690dc042995ef922b38a278d7a02effc2f1
mingz <305603665@qq.com>
2016-08-03 18:04:32 +0800

    init

--- helper/mv_to_qianzhan.py
+++ helper/mv_to_qianzhan.py
@@ -12,6 +12,10 @@ mongo_client = pymongo.MongoClient(MONGO_URI)
 db = mongo_client["qianzhan"]
 
 if __name__ == "__main__":
-    cur = db.company_info_items_gaoxin.find({}, {"_id": 0}).batch_size(50)
+    cur = db.company_info_items_detail.find(
+        {},
+        {"_id": 0, "searchitemsite": 0, "searchitemdftz": 0, "getcommentlist": 0, "searchitemnbinfo": 0,
+         "SearchItemCCXX": 0, "searchitemnb": 0}
+    ).batch_size(50)
     for item in cur:
-        db.company_info_items_detail.update({'company_name': item['company_name']}, {'$set': item}, True, True)
+        db.company_info_items_base.update({'company_name': item['company_name']}, {'$set': item}, True, True)
