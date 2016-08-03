0045a1feeb34e3b56d763b17b2c758c726bc7192
mingz <305603665@qq.com>
2016-08-03 19:28:23 +0800

    init

--- helper/init_redis.py
+++ helper/init_redis.py
@@ -3,16 +3,14 @@ __author__ = 'zhaojm'
 
 import pymongo
 import logging
+import redis
+import os
 
 # MONGO
 MONGO_URI = "localhost:27017"
-# MONGO_NEEQ_DB = "neeq"
 
 mongo_client = pymongo.MongoClient(MONGO_URI)
 db = mongo_client["qianzhan"]
-
-import redis
-import os
 
 REDIS_HOST = os.getenv("REDIS_HOST", '127.0.0.1')
 REDIS_PORT = os.getenv("REDIS_PORT", 6379)
@@ -20,18 +18,17 @@ REDIS_PORT = os.getenv("REDIS_PORT", 6379)
 redis_client = redis.StrictRedis(REDIS_HOST, REDIS_PORT, db=0)
 
 
-class CompanyDB(object):
+class QianzhanDB(object):
     def __init__(self):
         pass
 
     @staticmethod
-    def upsert_company(item):
-        logging.info("<MONGO> %s" % item)
-        db.company_info_items.update({'company_name': item['company_name']}, {'$set': item}, True, True)
+    def get_companys_base():
+        return db.company_info_items_base.find().batch_size(50)
 
     @staticmethod
-    def get_companys():
-        return db.company_info_items.find().batch_size(50)
+    def get_companys_detail():
+        return db.company_info_items_detail.find().batch_size(50)
 
 
 class RedisClient(object):
@@ -39,20 +36,36 @@ class RedisClient(object):
         pass
 
     @staticmethod
-    def set_company_name_key(company_name):
-        redis_client.hset("company_name", company_name, True)
+    def set_company_name_base_key(company_name):
+        redis_client.hset("company_name_base", company_name, True)
 
     @staticmethod
-    def get_company_name_key(company_name):
-        return redis_client.hexists("company_name", company_name)
+    def get_company_name_base_key(company_name):
+        return redis_client.hexists("company_name_base", company_name)
 
     @staticmethod
-    def set_company_url_key(url):
-        redis_client.hset("company_url", url, True)
+    def set_company_url_base_key(url):
+        redis_client.hset("company_url_base", url, True)
 
     @staticmethod
-    def get_company_url_key(url):
-        return redis_client.hget("company_url", url)
+    def get_company_url_base_key(url):
+        return redis_client.hget("company_url_base", url)
+
+    @staticmethod
+    def set_company_name_detail_key(company_name):
+        redis_client.hset("company_name_detail", company_name, True)
+
+    @staticmethod
+    def get_company_name_detail_key(company_name):
+        return redis_client.hexists("company_name_detail", company_name)
+
+    @staticmethod
+    def set_company_url_detail_key(url):
+        redis_client.hset("company_url_detail", url, True)
+
+    @staticmethod
+    def get_company_url_detail_key(url):
+        return redis_client.hget("company_url_detail", url)
 
 
 if __name__ == "__main__":
@@ -63,8 +76,12 @@ if __name__ == "__main__":
     sys.setdefaultencoding("utf-8")
     print "sys default encoding: ", sys.getdefaultencoding()
 
-
-    cur = CompanyDB.get_companys()
+    cur = QianzhanDB.get_companys_base()
     for company in cur:
         logging.info(company['company_name'])
-        RedisClient.set_company_name_key(company['company_name'])
+        RedisClient.set_company_name_base_key(company['company_name'])
+
+    cur = QianzhanDB.get_companys_detail()
+    for company in cur:
+        logging.info(company['company_name'])
+        RedisClient.set_company_name_detail_key(company['company_name'])
