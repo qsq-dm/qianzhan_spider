9aad265e11d3b62f05e115a4828961e8c50a0104
mingz <305603665@qq.com>
2016-08-12 12:16:59 +0800

    init

--- shell/run_spider_1000_base_not_login.sh
+++ shell/run_spider_1000_base_not_login.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /home/apps/qianzhan_spider
 dt=$(date "+%Y-%m-%d")
-path_to_log="log/""$dt""_qianzhan_spider_1000_not_login.log"
+path_to_log="log/""$dt""_qianzhan_spider_base_1000_not_login.log"
 echo $path_to_log
-python qianzhan_spider_1000_not_login/setup.py &
+python qianzhan_spider_1000_base_not_login/setup.py &
