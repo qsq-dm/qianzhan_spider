c7a60c3d64abe19e934806a30dc7836352fecacd
mingz <305603665@qq.com>
2016-08-15 17:48:51 +0800

    init

--- qianzhan_spider_1000_base_not_login/config.py
+++ qianzhan_spider_1000_base_not_login/config.py
@@ -30,4 +30,5 @@ default_headers = {
 # proxies = "http://120.198.248.97:80"
 # proxies = "http://103.27.24.234:80"
 # proxies = "http://103.27.24.236:80"
-proxies = "http://203.210.8.41:80"
+# proxies = "http://203.210.8.41:80"
+proxies = "http://59.127.154.78:80"
