296244fd9f2fd3303ec598b933f9bcbedb70e681
mingz <305603665@qq.com>
2016-08-24 18:46:13 +0800

    init

--- qianzhan_spider_chinabidding_detail_login/config.py
+++ qianzhan_spider_chinabidding_detail_login/config.py
@@ -32,4 +32,5 @@ default_headers = {
 # proxies = "http://111.13.7.42:80"
 # proxies = "http://124.88.67.7:843"
 # proxies = "http://112.17.250.78:8080"
-proxies = "http://115.208.89.195:8998"
+# proxies = "http://115.208.89.195:8998"
+proxies = "http://112.95.57.10:9999"
